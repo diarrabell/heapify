35ac6b05386e5ab402c28551b4647f4885ec7093
Anya Greenberg <durberg7@gmail.com>
2018-11-05 20:43:15 -0500

    asdas

--- src/Heap.java
+++ src/Heap.java
@@ -1,11 +1,26 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD:src/com/game/Heap.java
-package com.game; //comment out before submitting
-=======
-
->>>>>>> 680f64bf7be85d39dab8e90fe1b23dc6b0d7d524:src/Heap.java
-
->>>>>>> d3f4dec7fb670091a87eba60697f79378bd5aca7
 public class Heap{
+    /*
+    takes an array of integers as input and converts it to a max heap
+     */
+    public int[] heapify(int[] arr) {
+
+        return arr;
+    }
+
+    /*
+    takes an integer array and sorts it  (ascending order)
+     */
+    public int[] heapsort(int[] arr) {
+        return arr;
+    }
+
+    public String printHeap(int[] arr) {
+        String s= "";
+
+        for (int i=0; i<arr.length; i++) {
+            s = s + arr[i] + " ";
+        }
+
+        return s;
+    }
 }
