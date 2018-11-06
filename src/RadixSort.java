d4943158d1cad8a274cf01ced2d30f7fde3943b5
diarrabell <dbell5@u.rochester.edu>
2018-11-05 20:49:51 -0500

    hbubri

--- src/RadixSort.java
+++ src/RadixSort.java
@@ -1,6 +1,8 @@
 public class RadixSort {
     //TODO: function to get max value in array
+    public static void maxValue(int[] array, int num){}
 
     //TODO: function to sort array using radix sort
+
 
 }
