053cad241a69cf8d09268ab136d027c36bc00158
Anya Greenberg <durberg7@gmail.com>
2018-11-10 21:02:59 -0500

    one swap works

--- src/Heap.java
+++ src/Heap.java
@@ -1,10 +1,73 @@
 public class Heap{
+    int parent, lChild, rChild;
     /*
     takes an array of integers as input and converts it to a max heap
      */
     public int[] heapify(int[] arr) {
-
+        getBottomRight(arr);
+        compare(arr);
         return arr;
+    }
+
+    /*
+    compares parent to its children
+     */
+    public void compare(int[] arr) {
+        if(rChild != -1) {
+            if (arr[parent]<arr[lChild] || arr[parent]<arr[rChild]) {
+                if (arr[lChild] > arr[rChild]) {
+                    swap(arr, lChild, parent);
+                } else {
+                    swap(arr, rChild, parent);
+                }
+            }
+        } else {
+            if (arr[parent]<arr[lChild]) {
+                swap(arr,parent,lChild);
+            }
+        }
+
+    }
+
+    public int getBottomRight (int[] arr){
+        int i=0;
+
+        while (lChild<arr.length && rChild<arr.length) {
+            parent = i; //(i - 1) / 2;
+            lChild = 2 * i + 1;
+            rChild = 2 * i + 2;
+//            System.out.println(arr.length);
+//            System.out.println(i + ":" + parent + "-" + lChild + "-" + rChild);
+            i++;
+        }
+
+        if(rChild>=arr.length) {
+            rChild = -1;
+//            System.out.println(i + ":" + parent + "-" + lChild + "-" + rChild);
+        }
+
+        if(rChild>lChild && rChild<arr.length){
+            return rChild;
+        }
+
+        if(lChild<arr.length) {
+            return lChild;
+        }
+
+        //indicates there is no node
+        return -1;
+    }
+
+
+
+
+    /*
+    swaps indexes of a and b
+     */
+    public void swap (int[] arr, int a, int b) {
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
     }
 
     /*
@@ -14,6 +77,9 @@ public class Heap{
         return arr;
     }
 
+    /*
+    prints heap as array
+     */
     public String printHeap(int[] arr) {
         String s= "";
 
--- src/HeapTest.java
+++ src/HeapTest.java
@@ -3,21 +3,20 @@ public class HeapTest {
         Heap heap = new Heap();
         int[] arr = {5, 18, 3, 25, 27, 45, 97, 88, 26, 16, 49, 67};
         int[] arr2 = {15, 99, 3, 77, 27, 45, 7, 88, 26, 5};
-        int[] heapArr,sortArr;
-
+//        int[] heapArr,sortArr;
+//
 //        heapArr = heap.heapify(arr);
-        heap.printHeap(arr);
-
+//        heap.printHeap(heapArr);
+//
 //        sortArr = heap.heapsort(arr2);
-//        heap.printHeap(arr2);
+//        heap.printHeap(sortArr);
 
-        String s= "";
+        /*
+        temporary testing
+        */
 
-        for (int i=0; i<arr.length; i++) {
-            s = s + arr[i] + " ";
-        }
-
-        System.out.print(s);
-        
+        System.out.println(heap.printHeap(arr));
+        heap.heapify(arr);
+        System.out.println(heap.printHeap(arr));
     }
 }
