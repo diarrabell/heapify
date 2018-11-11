1145fa8f59b3f41985a466e81cd7863588cf1136
Anya Greenberg <durberg7@gmail.com>
2018-11-10 22:14:17 -0500

    done

--- src/Heap.java
+++ src/Heap.java
@@ -1,18 +1,38 @@
+import java.util.ArrayList;
+
 public class Heap{
-    int parent, lChild, rChild;
+    int parent, lChild, rChild, max=0;
     /*
     takes an array of integers as input and converts it to a max heap
      */
     public int[] heapify(int[] arr) {
-        getBottomRight(arr);
-        compare(arr);
+        max=0;
+
+        for(int i=0; i<arr.length-1; i++) {
+            if (arr[i+1]> max) {
+                max = arr[i+1];
+            }
+        }
+
+        while (arr[0]< max) {
+            int i=0;
+            lChild=0;
+            rChild=0;
+            parent=0;
+            while (lChild != -1 || (rChild != -1 && lChild != -1)) {
+                compare(arr, i);
+                i++;
+            }
+        }
+
         return arr;
     }
 
     /*
-    compares parent to its children
+    compares parent to its child
      */
-    public void compare(int[] arr) {
+    public void compare(int[] arr, int i) {
+        getChild(arr,i);
         if(rChild != -1) {
             if (arr[parent]<arr[lChild] || arr[parent]<arr[rChild]) {
                 if (arr[lChild] > arr[rChild]) {
@@ -21,29 +41,34 @@ public class Heap{
                     swap(arr, rChild, parent);
                 }
             }
-        } else {
+        } else if (lChild !=-1){
             if (arr[parent]<arr[lChild]) {
                 swap(arr,parent,lChild);
             }
         }
-
     }
 
-    public int getBottomRight (int[] arr){
-        int i=0;
+    /*
+    gets the higher numbered child of the specified node/index
+     */
+    public int getChild (int[] arr, int i){
+        if(lChild<arr.length && rChild<arr.length && parent<arr.length) {
+            parent = i;
 
-        while (lChild<arr.length && rChild<arr.length) {
-            parent = i; //(i - 1) / 2;
-            lChild = 2 * i + 1;
-            rChild = 2 * i + 2;
-//            System.out.println(arr.length);
-//            System.out.println(i + ":" + parent + "-" + lChild + "-" + rChild);
+            if(i<arr.length) {
+                lChild = 2 * i + 1;
+                rChild = 2 * i + 2;
+            }
+
             i++;
         }
 
         if(rChild>=arr.length) {
             rChild = -1;
-//            System.out.println(i + ":" + parent + "-" + lChild + "-" + rChild);
+        }
+
+        if(lChild>=arr.length) {
+            lChild = -1;
         }
 
         if(rChild>lChild && rChild<arr.length){
@@ -58,9 +83,6 @@ public class Heap{
         return -1;
     }
 
-
-
-
     /*
     swaps indexes of a and b
      */
@@ -74,7 +96,43 @@ public class Heap{
     takes an integer array and sorts it  (ascending order)
      */
     public int[] heapsort(int[] arr) {
-        return arr;
+        int[] sorted = new int[arr.length];
+
+        while(arr.length>0) {
+            ArrayList<Integer> temp;
+            heapify(arr);
+            swap(arr, 0, arr.length - 1);
+            sorted[arr.length-1] = arr[arr.length-1];
+            temp = toArrayList(arr);
+            temp.remove(arr.length - 1);
+            arr = toArray(temp);
+        }
+
+        return sorted;
+    }
+
+    /*
+    turns arraylist into array to be passed into sort and heapify methods
+     */
+    public int[] toArray (ArrayList<Integer> arr) {
+        int[] temp = new int[arr.size()];
+
+        for (int i=0; i<arr.size(); i++) {
+            temp[i] = arr.get(i);
+        }
+        return temp;
+    }
+
+    /*
+    turns array into array list to easily remove last item
+     */
+    public ArrayList<Integer> toArrayList(int[] arr) {
+        ArrayList<Integer> temp = new ArrayList<>();
+
+        for (int i=0; i<arr.length; i++) {
+            temp.add(arr[i]);
+        }
+        return temp;
     }
 
     /*
--- src/HeapTest.java
+++ src/HeapTest.java
@@ -3,20 +3,12 @@ public class HeapTest {
         Heap heap = new Heap();
         int[] arr = {5, 18, 3, 25, 27, 45, 97, 88, 26, 16, 49, 67};
         int[] arr2 = {15, 99, 3, 77, 27, 45, 7, 88, 26, 5};
-//        int[] heapArr,sortArr;
-//
-//        heapArr = heap.heapify(arr);
-//        heap.printHeap(heapArr);
-//
-//        sortArr = heap.heapsort(arr2);
-//        heap.printHeap(sortArr);
+        int[] heapArr,sortArr;
 
-        /*
-        temporary testing
-        */
+        heapArr = heap.heapify(arr);
+        System.out.println(heap.printHeap(heapArr));
 
-        System.out.println(heap.printHeap(arr));
-        heap.heapify(arr);
-        System.out.println(heap.printHeap(arr));
+        sortArr = heap.heapsort(arr2);
+        System.out.println("sorted: " + heap.printHeap(sortArr));
     }
 }
